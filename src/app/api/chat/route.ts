49a50c7c591715d0f9c6e73929bc7b1260ef52b3
didemkkasslan <didem@spiky.ai>
2023-07-24 00:18:24 +0300

    Add rate limit

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,4 +1,6 @@
-import { StreamingTextResponse, LangChainStream, Message } from 'ai';
+import { kv } from '@vercel/kv';
+import { Ratelimit } from '@upstash/ratelimit';
+import { StreamingTextResponse, LangChainStream } from 'ai';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 import { BufferMemory } from 'langchain/memory';
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
@@ -11,6 +13,36 @@ export const runtime = 'edge';
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
+
+  // Save the chat history to KV
+  await kv.set('chat_history', JSON.stringify(messages));
+
+  if (
+    process.env.NODE_ENV != 'development' &&
+    process.env.KV_REST_API_URL &&
+    process.env.KV_REST_API_TOKEN
+  ) {
+    const ip = req.headers.get('x-forwarded-for');
+    const ratelimit = new Ratelimit({
+      redis: kv,
+      limiter: Ratelimit.slidingWindow(2, '1 d'), // 2 requests per day per IP
+    });
+
+    const { success, limit, reset, remaining } = await ratelimit.limit(
+      `portfolio_ratelimit_${ip}`,
+    );
+
+    if (!success) {
+      return new Response('You have reached your request limit for the day.', {
+        status: 429,
+        headers: {
+          'X-RateLimit-Limit': limit.toString(),
+          'X-RateLimit-Remaining': remaining.toString(),
+          'X-RateLimit-Reset': reset.toString(),
+        },
+      });
+    }
+  }
 
   const memory = new BufferMemory({
     memoryKey: 'chat_history',
